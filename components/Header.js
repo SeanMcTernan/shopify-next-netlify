d869d82474b4b8698c1ee6c8b887dfdf689fd069
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2022-06-27 15:26:49 -0600

    Update Script Location

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,10 @@
 import Link from 'next/link';
-import Script from 'next/script'
 import { useAppContext } from 'state';
 
 export default function Header(props) {
   const { itemQuantities } = useAppContext();
   return (
     <header className="app-header">
-      <Script
-        id="search"
-        src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"
-        onLoad={() => {
-          algoliasearchNetlify({
-            appId: 'VXS80WCPYY',
-            apiKey: process.env.NEXT_PUBLIC_ALGOLIA_FRONTEND_API_KEY,
-            siteId: '6e0fc743-62a2-41f9-8b6b-de54d5921004',
-            branch: 'main',
-            selector: 'div#search',
-          })
-        }}
-      />
       <div>
         <img src={'https://www.lifefitness.com/resource/crblob/1164/260e4ad4ec1b7642864fbe5a149d147c/logo-svg-data.svg'} alt={''} />
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import { AppWrapper } from '../state.js'; // import based on where you put it
 import '@styles/globals.css';
+import Script from 'next/script'
 
 export function Application({ Component, pageProps }) {
   return (
     <AppWrapper>
       <Component {...pageProps} />
+      <Script
+        id="search"
+        src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"
+        onLoad={() => {
+          algoliasearchNetlify({
+            appId: 'VXS80WCPYY',
+            apiKey: process.env.NEXT_PUBLIC_ALGOLIA_FRONTEND_API_KEY,
+            siteId: '6e0fc743-62a2-41f9-8b6b-de54d5921004',
+            branch: 'main',
+            selector: 'div#search',
+          })
+        }}
+      />
     </AppWrapper>
   );
 }
