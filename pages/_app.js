e51e9b9f236c8d12599f0a95278949afdafbb074
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2022-06-28 16:14:20 -0600

    remove script from file

--- pages/_app.js
+++ pages/_app.js
@@ -6,19 +6,6 @@ export function Application({ Component, pageProps }) {
   return (
     <AppWrapper>
       <Component {...pageProps} />
-      <Script
-        id="search"
-        src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"
-        onLoad={() => {
-          algoliasearchNetlify({
-            appId: 'VXS80WCPYY',
-            apiKey: process.env.NEXT_PUBLIC_ALGOLIA_FRONTEND_API_KEY,
-            siteId: '6e0fc743-62a2-41f9-8b6b-de54d5921004',
-            branch: 'main',
-            selector: 'div#search',
-          })
-        }}
-      />
     </AppWrapper>
   );
 }
